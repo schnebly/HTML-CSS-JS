8118a989f8d8b959ed526f3d412dbe836955d0a2
James Schnebly <jameschnebly@gmail.com>
2017-09-28 21:59:07 -0700

    Add files via upload

--- new_proj/JS/script.js
+++ new_proj/JS/script.js
@@ -1,6 +1,5 @@
 //SearchBar Handler
 $(function(){
-
 	var searchField = $('#query');
 	var icon = $('#search-btn');
 
@@ -56,26 +55,83 @@ function search()
 			console.log(data);
 
 			$.each(data.items, function(i, item){
-				//Get Ouptput
+				//Get Output
 				var output = getOutput(item);
 
 				//Display Results
 				$('#results').append(output);
 			});
+			
+			var buttons = getButtons(prevPageToken, nextPageToken);
+			
+			//display buttons
+			$('#buttons').append(buttons);
 		}
 
 	);
 }
 
+//Build Output
 function getOutput(item){
 
 	
 	var videoId = item.id.vidioId;
 	var title = item.snippet.title;
-	var description = item.snippet.descritption;
+	var description = item.snippet.description;
 	var thumb = item.snippet.thumbnails.high.url;
 	var channelTitle = item.snippet.channelTitle;
 	var videoDate = item.snippet.publishedAt;
 
+	
 	//Build Output String
+	var output = '<li>' + 
+	'<div class = "list-left">' + 
+	'<img src = "'+thumb+'">' + 
+	'</div>' + 
+	'<div class = "list-right">' + 
+	'<h3>'+title+'</h3>' +
+	'<small>By <span class = "cTitle">' +channelTitle+'</span> on '+videoDate+'</small>' + 
+	'<p>'+description+'</p>' + 
+	'</div>' +
+	'</li>' + 
+	'<div class = "clearfix"></div>' + 
+	'';
+
+	return output;
 }
+
+//Build Buttons
+function getButtons(prevPageToken, nextPageToken){
+	if(!prevPageToken){
+		var btnoutput = '<div class= "button-container">' + 
+		'<button id = "next-button" class = "paging-button" data-token = "'+nextPageToken+'" data-query="'+q+'"' + 
+		'onclick = "nextPage();">Next Page</button></div>';
+	}
+	else{
+		var btnoutput = '<div class= "button-container">' + 
+		'<button id = "next-button" class = "paging-button" data-token = "'+prevPageToken+'" data-query="'+q+'"' + 
+		'onclick = "prevPage();">Prev Page</button>'
+		'<button id = "next-button" class = "paging-button" data-token = "'+nextPageToken+'" data-query="'+q+'"' + 
+		'onclick = "nextPage();">Next Page</button></div>';
+	}
+	
+	return btnoutput;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
